a8173a89bc665e382a1318163f318e0fa459d906
Emmi Takkinen <emmi.takkinen@koha-suomi.fi>
2022-06-06 12:26:24 +0300

    Create symlinks automatically when running set-development.sh

--- set-development.sh
+++ set-development.sh
@@ -11,4 +11,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/OKMStats" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OKMStats
 ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/OKMStats.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OKMStats.pm
 
+rm $kohadir/misc/cronjobs/generate_okm_annual_statistics.pl
+ln -s $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OKMStats/cronjobs/generate_okm_annual_statistics.pl $kohadir/misc/cronjobs/generate_okm_annual_statistics.pl
+
+rm $kohadir/misc/cronjobs/update_biblio_data_elements.pl
+ln -s $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/OKMStats/cronjobs/update_biblio_data_elements.pl $kohadir/misc/cronjobs/update_biblio_data_elements.pl
+
 perl $kohadir/misc/devel/install_plugins.pl
