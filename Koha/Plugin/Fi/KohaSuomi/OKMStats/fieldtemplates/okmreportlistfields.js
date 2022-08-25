7bbb583c4d2297bd7a7e8ea9aae51e9094645d9a
lmstrand <lmstrand@gmail.com>
2022-08-25 10:45:27 +0300

    Do not filter okm report creation timestamps, minor ui fixes

--- Koha/Plugin/Fi/KohaSuomi/OKMStats/fieldtemplates/okmreportlistfields.js
+++ Koha/Plugin/Fi/KohaSuomi/OKMStats/fieldtemplates/okmreportlistfields.js
@@ -1,8 +1,8 @@
 const okmreportlistfields = [
     { label: "Id", key: "0", sortable: true },
-    { label: "Statistical groups", key: "1" },
+    { label: "Statistical groups", key: "1" , sortable: true},
     { label: "Alkupvm", key: "2" , sortable: true},
-    { label: "Loppupvm", key: "3" },
-    { label: "Luotu", key: "4" , sortable: true}
+    { label: "Loppupvm", key: "3" , sortable: true},
+    { label: "Luotu", key: "4" , sortable: true, filter: false}
    
 ]
